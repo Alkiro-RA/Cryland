7d4444562e37aadd6c8cc63fdc92e9cd7c018b78
Kyuba98 <jakub.krzeminski1998@gmail.com>
2023-12-22 16:20:27 +0100

    update battle_system.php & index.php

--- game/battle/battle_system.php
+++ game/battle/battle_system.php
@@ -2,11 +2,19 @@
 // Fetch player and enemy data from the database based on the battle scenario
 // For example, select player and enemy based on their IDs
 require_once ("../../data/db.php");
+require_once("../../other/authorization.php");
 
 
 try {
-    // Fetch player data by ID (change '1' to the player's actual ID)
-    $player_id = 1;
+    // Fetch user data by ID
+    $user_id = $_SESSION['user_id'];
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE id = :id");
+    $stmt->bindParam(':id', $user_id);
+    $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Fetch player data by ID
+    $player_id = $user['charactersid'];
     $stmt = $pdo->prepare("SELECT * FROM characters WHERE id = :id");
     $stmt->bindParam(':id', $player_id);
     $stmt->execute();
@@ -22,102 +30,153 @@ try {
     // Check if data was fetched successfully
     if ($player && $enemy) {
         // Do something with $player and $enemy data
-        var_dump($player); // Example: Output player data
-        var_dump($enemy); // Example: Output enemy data
+        //var_dump($player); // Example: Output player data
+        //var_dump($enemy); // Example: Output enemy data
     } else {
         echo "Player or enemy not found!";
+    }
+    $player_action_done = false;
+    $paralysis_bomb = 0;
+    // Checking if player and enemy are alive
+    if ($player['health'] > 0 && $enemy['health'] > 0) {
+        // Step 1: Player's Turn
+        if ($player['health'] > 0) {
+            // Display options for the player (attack, use potion, etc.)
+            // Get player's action (from form submission, for instance)
+            if (isset($_GET['action'])) {
+                $action = $_GET['action'];
+
+                // Handle different actions based on the parameter
+                switch ($action) {
+                    case 'attack':
+                        $dmg = $player['attack'] - $enemy['defense'];
+                        if ($dmg < 1) {
+                            $dmg = 1;
+                        }
+                        $enemy['health'] = $enemy['health'] - $dmg;
+                        // Handle attack action
+                        // Perform your attack logic here
+                        $player_action_done = true;
+                        break;
+
+                    case 'potion':
+                        if ($player['potion']>0)
+                        {
+                            $player['health'] = $player['health'] + 10;
+                            if($player['health'] > $player['maxhealth']){
+                                $player['health'] = $player['maxhealth'];
+                            }
+                            $player['potion'] = $player['potion'] - 1;
+                            // Handle potion action
+                            // Perform potion-related logic here
+                            $player_action_done = true;
+                        }
+
+                        break;
+
+                    case 'consumable':
+                        if($player['consumable']>0)
+                        {
+                            // Aplying paralysis on enemy
+                            $paralysis_bomb = 2;
+                            // Handle consumable action
+                            // Perform consumable-related logic here
+                            $player_action_done = true;
+                            $player['consumable'] = $player['consumable'] - 1;
+                        }
+
+                        break;
+
+                    case 'consumable_2':
+                        if($player['consumable_2']>0)
+                        {
+                            // Bomb that ignore defense
+                            $dmg = $player['attack']*2;
+                            $enemy['health'] = $enemy['health'] - $dmg;
+                            $player_action_done = true;
+                            $player['consumable_2'] = $player['consumable_2'] - 1;
+                        }
+
+
+
+                        break;
+
+                    default:
+                        // Handle invalid action or no action provided
+                        break;
+                }
+            } else {
+                // No action parameter provided
+            }
+            // Perform action based on player's choice
+            // Update player/enemy health, check for special actions, etc.
+
+            // Check if the enemy survived the attack
+            if ($enemy['health'] <= 0) {
+                // Enemy defeated, handle victory
+                // Break the loop or perform necessary actions
+
+            } else if ($player_action_done) {
+                // Step 2: Enemy's Turn
+                // Enemy attacks the player
+                // Update player's health based on enemy's attack power
+                if ($paralysis_bomb > 0)
+                {
+                    $paralysis_bomb --;
+                }
+                else{
+                    $dmg = $enemy['attack'] - $player['defense'];
+                    if($dmg < 1) $dmg = 1;
+                    $player['health'] = $player['health']- $dmg;
+                }
+                // Check if the player survived the attack
+                if ($player['health'] <= 0) {
+                    // Player defeated, handle defeat
+                    // Break the loop or perform necessary actions
+                }
+            }
+        }
+    }
+    // Update player and enemy data in the database after battle
+    if ($player && $enemy) {
+        // Update player's data in the database
+        $stmt = $pdo->prepare("UPDATE characters SET health = :health, potion = :potion, consumable = :consumable, consumable_2 = :consumable_2 WHERE id = :id");
+        $stmt->bindParam(':health', $player['health']);
+        $stmt->bindParam(':potion', $player['potion']);
+        $stmt->bindParam(':consumable', $player['consumable']);
+        $stmt->bindParam(':consumable_2',$player['consumable_2']);
+        $stmt->bindParam(':id', $player_id);
+        $stmt->execute();
+
+        // Update enemy's data in the database
+        $stmt = $pdo->prepare("UPDATE enemies SET health = :health WHERE id = :id");
+        $stmt->bindParam(':health', $enemy['health']);
+        $stmt->bindParam(':id', $enemy_id);
+        $stmt->execute();
     }
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-$player_health = $player['health'];
-$enemy_health = $enemy['health'];
-$paralysis_bomb = 0;
-// Loop until either player or enemy health reaches 0
-while ($player_health > 0 || $enemy_health > 0) {
-    // Step 1: Player's Turn
-    if ($player_health > 0) {
-        // Display options for the player (attack, use potion, etc.)
-        // Get player's action (from form submission, for instance)
-        if (isset($_GET['action'])) {
-            $action = $_GET['action'];
-
-            // Handle different actions based on the parameter
-            switch ($action) {
-                case 'attack':
-                    $dmg = $player['attack'] - $enemy['defense'];
-                    if ($dmg < 1) {
-                        $dmg = 1;
-                    }
-                    $enemy_health = $enemy_health - $dmg;
-                    // Handle attack action
-                    // Perform your attack logic here
-                    break;
-
-                case 'potion':
-                    $player_health = $player_health + 10;
-                    if($player_health > $player['health']){
-                        $player_health = $player['health'];
-                    }
-                    // Handle potion action
-                    // Perform potion-related logic here
-                    break;
-
-                case 'consumable':
-                    // Aplying paralysis on enemy
-                    $paralysis_bomb = 2;
-                    // Handle consumable action
-                    // Perform consumable-related logic here
-                    break;
-
-                case 'consumable_2':
-                    // Bomb that ignore defense
-                    $dmg = $player['attack']*2;
-                    $enemy_health = $enemy_health - $dmg;
 
 
-                    break;
+$player_action_done = false;
 
-                default:
-                    // Handle invalid action or no action provided
-                    echo "Invalid action!";
-                    break;
-            }
-        } else {
-            // No action parameter provided
-            echo "No action specified!";
-        }
-        // Perform action based on player's choice
-        // Update player/enemy health, check for special actions, etc.
+// Generate the updated battle information HTML
+$battleInfoHTML = '<div class="battle-info">';
+$battleInfoHTML .= '<div class="player-info">';
+$battleInfoHTML .= '<h2>' . $player['name'] . '</h2>';
+$battleInfoHTML .= '<p>HP: ' . $player['health'] . ' / ' . $player['maxhealth'] . '</p>';
+$battleInfoHTML .= '<p>Potions: ' . $player['potion'] . '</p>';
+$battleInfoHTML .= '<p>Paralysis bombs: ' . $player['consumable'] . '</p>';
+$battleInfoHTML .= '<p>Fire bombs: ' . $player['consumable_2'] . '</p>';
+$battleInfoHTML .= '</div>';
+$battleInfoHTML .= '<div class="enemy-info">';
+$battleInfoHTML .= '<h2>' . $enemy['name'] . '</h2>';
+$battleInfoHTML .= '<p>HP: ' . $enemy['health'] . ' / ' . $enemy['maxhealth'] . '</p>';
+// Add more enemy information as needed
+$battleInfoHTML .= '</div>';
+$battleInfoHTML .= '</div>';
 
-        // Check if the enemy survived the attack
-        if ($enemy_health <= 0) {
-            // Enemy defeated, handle victory
-            // Break the loop or perform necessary actions
-            echo "You won!!";
-            break;
-
-        } else {
-            // Step 2: Enemy's Turn
-            // Enemy attacks the player
-            // Update player's health based on enemy's attack power
-            if ($paralysis_bomb > 0)
-            {
-                $paralysis_bomb --;
-            }
-            else{
-                $dmg = $enemy['atack'] - $player['defense'];
-                if($dmg < 1) $dmg = 1;
-                $player_health =- $dmg;
-            }
-            // Check if the player survived the attack
-            if ($player_health <= 0) {
-                // Player defeated, handle defeat
-                // Break the loop or perform necessary actions
-                echo "You loose";
-                break;
-            }
-        }
-    }
-}
+// Echo or return the HTML content
+echo $battleInfoHTML; // This will output the HTML content directly
 ?>
--- game/battle/index.php
+++ game/battle/index.php
@@ -11,6 +11,65 @@ require_once("../../other/authorization.php");
     <meta charset="UTF-8">
     <title> Battle </title>
     <link rel="stylesheet" href="../../other/navbar.css">
+    <style>
+        /* Updated styles */
+
+        .battle-info {
+            display: flex;
+            justify-content: center;
+            align-items: stretch;
+            gap: 50px; /* Max distance between player and enemy */
+            margin-top: 20px;
+            margin-bottom: 20px; /* Spacing between battle info and buttons */
+        }
+
+        .player-info, .enemy-info {
+            width: 20%; /* Adjust width as needed */
+            border: 5px solid #ccc;
+            padding: 10px;
+        }
+
+        .buttons {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .buttons button {
+            margin: 5px;
+            padding: 10px 20px;
+        }
+    </style>
+    <script>
+        function loadBattleInfo() {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("battleInfo").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", "battle_system.php", true); // Replace with the actual path
+            xhttp.send();
+        }
+
+        function performAction(action) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    loadBattleInfo(); // Refresh battle information after performing the action
+                }
+            };
+            xhttp.open("GET", "battle_system.php?action=" + action, true); // Replace with the actual path
+            xhttp.send();
+        }
+
+        // Load battle information when the page loads
+        window.onload = function() {
+            loadBattleInfo();
+            // Refresh battle information every few seconds (example: every 5 seconds)
+            setInterval(loadBattleInfo, 5000); // Adjust the time interval as needed
+        };
+    </script>
 </head>
 
 <body>
@@ -22,8 +81,15 @@ require_once("../../other/authorization.php");
         <a class="logout" href="/cryland/other/logout.php">Logout</a>
     </div>
     <!-- Battle -->
-    <div>
-        <h1> Tu sie nakurwiasz na tury </h1>
+    <div id="battleInfo">
+        <!-- Battle information will be displayed here -->
+    </div>
+
+    <div class="buttons">
+        <button onclick="performAction('attack')">Attack</button>
+        <button onclick="performAction('potion')">Use Potion</button>
+        <button onclick="performAction('consumable')">Use paralysis bomb</button>
+        <button onclick="performAction('consumable_2')">Use fire bomb</button>
     </div>
 </body>
 
