b6e663d2025eaf97de4f593c69750bb37c419847
Alkiro-RA <alkiewiczroger@gmail.com>
2023-12-30 18:10:03 +0100

    defeated bosses show in the ranking

--- ranking/ranking_controller.php
+++ ranking/ranking_controller.php
@@ -4,7 +4,7 @@ try {
     // Db configuration
     require_once("../data/db.php");
     // Get characters' data from db
-    $sql = "SELECT name, level FROM Characters";
+    $sql = "SELECT name, level, duel_won FROM Characters";
     $stmt = $pdo->prepare($sql);
     if (!$stmt->execute()) {
         throw new Exception();
@@ -14,16 +14,26 @@ try {
     usort($characters, function ($a, $b) {
         return $b['level'] <=> $a['level'];
     });
+    
+    // Make loop show top 10 players
     // Put data into HTML's table
     foreach ($characters as $char) {
-        echo 
+        $fallen_bosses = "";
+        $duel_count = $char['duel_won'];
+        // Check won duels
+        if ($duel_count > 0) {
+            $fallen_bosses .= ' Goblin King ';
+        }
+        if ($duel_count > 1) {
+            $fallen_bosses .= ' Smoczex Płaczex ';
+        }
+        echo
         "<tr>
             <td>{$char['name']}</td>
             <td>{$char['level']}</td>
-            <td></td>
+            <td>{$fallen_bosses}</td>
         </tr>";
     }
-} 
-catch (Exception) {
+} catch (Exception) {
     echo "User's fault.";
 }
